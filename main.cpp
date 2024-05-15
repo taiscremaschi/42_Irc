398206969de821c67a2d8f48646752aceb81c94e
taiscremaschi <taiscremaschi@gmail.com>
2024-05-15 17:33:35 +0100

    refatoracao em progresso

--- main.cpp
+++ main.cpp
@@ -7,57 +7,39 @@ int main(int ac, char **av)
     if(ac != 3)
         return (errorMsg("Wrong number of arguments"));
     int port = myPortConvertor(av[1]);
-    printf("%d\n", port);
     if(port == -1)
         return (errorMsg("Invalid port number"));
 
     // TODO: tratar das passwords
 
-
-
-    //crio uma variavel inteira com o socket e os parametros
-    //af_net é o dominio e sera usando o protocoloco ipv4 
-    //o sock_stream iéo type indicando uma conexao tcp ou sseja
-    //os dados serao trasmitidos de forma sequencial sem eroros erros
-    //ou duplicatas. ja o ultimo eh o protocolo que 0 indica que o sistam vai escolher 
-    //o protocolo apropriado automaticamente basendo no domonio e no tipo especificado
-
-    int myFirtSocket = socket(AF_INET, SOCK_STREAM, 0);
+    int myFirtSocket = socket(AF_INET, SOCK_STREAM, 0); //my notes 1
     if(myFirtSocket == -1){
-        std::cerr << "deu ruim\n";
+        std::cerr << "error in create server socket\n";
         return 1;
     }
-    //agr preciso configurar o endereco do servidor
-    sockaddr_in serverAddr;
+    sockaddr_in serverAddr; // now I need configure the server address (read help 2)
     serverAddr.sin_family = AF_INET; 
-    serverAddr.sin_addr.s_addr = INADDR_ANY; //
-    serverAddr.sin_port = htons(port); //porta padrao do irc 6667
+    serverAddr.sin_addr.s_addr = INADDR_ANY; 
+    serverAddr.sin_port = htons(port); 
 
-    //afr ligar o o socket ao endereço 
-    //A função bind() é utilizada para associar um endereço IP e uma porta a um socket.
-    // Isso é necessário para que o sistema operacional saiba onde direcionar os dados que chegam para essa porta específica.
     if(bind(myFirtSocket, (sockaddr*) &serverAddr, sizeof(serverAddr)) == -1) {
-        std::cerr << "erro ao fazer bind" << std::endl;
+        std::cerr << "erro ao fazer bind" << std::endl; //help 3
     }
-    //listen pro socket com o numero max de conexoes pendentes que o sisteman
-    //pode infileirar antes de começar a rejeitar novas conexoes
     if(listen(myFirtSocket, 5) == -1) {
-        std::cerr << "erro ao fazer listen\n";
+        std::cerr << "erro ao fazer listen\n"; // max_nbr of connections
     }
-  
     std::cout << "Servidor aguardando por conexões...\n";
 
     std::vector<ClientInfo> clients;
     clients.reserve(10); // Reserva espaço para 10 clientes
 
-    // Adicione o socket do servidor ao conjunto de descritores a serem monitorados por poll()
     std::vector<pollfd> fds;
     pollfd server;
     server.fd = myFirtSocket;
-    server.events = POLLIN;
-    fds.push_back(server);
+    server.events = POLLIN; 
+    fds.push_back(server); //add the server socket to the set of descriptors to be monitored by poll()
 
-    while (true) {
+    while (1) {
         int ret = poll(fds.data(), fds.size(), -1); // Espera indefinidamente por eventos
         if (ret < 0) {
             std::cerr << "Erro em poll()\n";
@@ -104,8 +86,6 @@ int main(int ac, char **av)
             }
         }
     }
-
-    // Fechar sockets, limpar recursos, etc...
     close(myFirtSocket);
     return 0;
 }
