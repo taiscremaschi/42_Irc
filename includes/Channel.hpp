9dd91dab5cbaf1c4869ce5af48711d75b57dca55
Pac4 <paulo.nicolau7@gmail.com>
2024-06-06 17:14:35 +0100

    Started working on support for invite-only channel mode

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -10,12 +10,20 @@ class Channel {
         std::string         _name;
         std::string         _topic;
         std::vector<Client*> _operators;
+		bool				_inviteOnly;
+		std::vector<Client*> _invitedClients;
     public:
         Channel(const std::string &name, Client *client);
         ~Channel();
+
         const std::string &getName() const;
 		const std::string &getTopic() const;
 		void setTopic(const std::string &topic);
+		bool isInviteOnly() const;
+		void setInviteOnly(bool inviteOnly);
+		bool isInvited(Client *client) const;
+		void addInvite(Client *client);
+
         std::vector<Client*>  &getOperator();
         bool addClient(Client *client);
         void removeClient(Client *client);
